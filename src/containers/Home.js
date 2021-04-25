fec268e70957e4a85bbe4a3d5e03898adf08bf07
GersonBenito <gersonbenito4@gmail.com>
2021-04-25 14:10:16 -0600

    button go to product

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+import { useHistory }from 'react-router-dom';
 
 const Home = () => {
+    const history = useHistory();
+
+    const goToProduct = () =>{
+        history.push('/productos');
+    }
     return (
-        <div>
-            <h2>Soy el home</h2>
-        </div>
+        <Contanedor>
+            <h2>Bienvenidos</h2>
+            <Button variant='contained' color='secondary' onClick={()=>goToProduct()} >Ir a productos</Button>
+        </Contanedor>
     )
 }
+
+const Contanedor = styled.div`
+    padding:20px;
+    height:85vh;
+`;
 
 export default Home
